3477e635d8f47145c369088dc2b71d4cbcefac36
Sumita <sumita.sami@gmail.com>
2013-07-19 04:06:28 +0000

    Copying the right dirs this time from prod educator_lab

--- test_educator_lab/mainlab/pickup_changes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-python2.7 manage.py schemamigration mainlab --auto
-echo ">>> If everything went well with that south migration, hit enter to continue. If not, hit Ctrl+C"
-read BLAH
-python2.7 manage.py migrate mainlab
-../apache2/bin/restart
